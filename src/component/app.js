514861c91c0bb56ac1b96c97e13681978338864a
alexlhanson <alexlhanson@gmail.com>
2018-11-10 14:30:44 -0800

    references #31 Adds links and files for containers

--- src/component/app.js
+++ src/component/app.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { MemoryRouter, Switch, Route, BrowserRouter } from 'react-router-dom';
 
+import LandingContainer from './landing/landing';
+import AuthContainer from './auth-form/auth-form';
+import SearchResultsContainer from './search-results/search-results';
+import ProfileContainer from './profile/profile';
+
 import * as route from '../actions/route';
 
 export class App extends React.Component {
@@ -10,9 +15,14 @@ export class App extends React.Component {
       <section className="app">
         <BrowserRouter>
           <div>
-            <h1>Hello App!</h1>
+            <h1>Hello Alex!</h1>
             <MemoryRouter>
-              <Switch location={{ pathname: this.props.route }}>
+              <Switch location={{ pathname: this.props.route }} >
+                <Route path='/landing' Component={LandingContainer} />
+                <Route path='/signup' Component={AuthContainer} />
+                <Route path='/login' Component={AuthContainer} />
+                <Route path='/search-results' Component={SearchResultsContainer} />
+                <Route path='/profile' Component={ProfileContainer} />
               </Switch>
             </MemoryRouter>
           </div>
