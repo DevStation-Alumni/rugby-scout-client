16589b902c73fe41508dcd40d1def7966a6a260a
alexlhanson <alexlhanson@gmail.com>
2018-11-10 16:43:46 -0800

    references 35 add reducer for switch route actions

--- src/reducers/route.js
+++ src/reducers/route.js
@@ -1,5 +1,10 @@
 export default (state = '/landing', { type, payload }) => {
   switch (type) {
+    case 'LANDING': return '/landing';
+    case 'SIGNUP': return '/signup';
+    case 'LOGIN': return '/login';
+    case 'SEARCH_RESULTS': return '/search-results';
+    case 'PROFILE': return '/profile';
     default: return state;
   }
 }
