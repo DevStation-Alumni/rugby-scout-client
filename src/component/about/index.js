e7d69366e94cd6d83e91395b51e7779daebe579a
Sharon Miller <sharon@sharonmillercreative.com>
2018-11-12 19:21:09 -0800

    add about content with flip photo option

--- src/component/about/index.js
+++ src/component/about/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Grid, Row, Col, Image } from 'react-bootstrap';
+import ReactCardFlipper from 'react-card-flipper';
 
 export class AboutContainer extends React.Component {
   constructor(props) {
@@ -8,8 +10,25 @@ export class AboutContainer extends React.Component {
   render() {
     return (
       <section className="about">
-        <h1>Get to know the team</h1>
-      </section>
+        <Grid>
+          <Row className="show-grid text-center">
+            <Col xs={12} sm={4} className="person-wrapper">
+              <ReactCardFlipper>
+                <div><Image src="https://s3.amazonaws.com/rugbyscout-two/connor_rs_about.png" className="profile-pic" /> </div>
+                <div><Image src="https://s3.amazonaws.com/rugbyscout-two/connor_rs_about.png" className="profile-pic" /> </div>
+              </ReactCardFlipper>
+            </Col>
+            <Col xs={12} sm={4} className="person-wrapper">
+              <Image src="https://s3.amazonaws.com/rugbyscout-two/sharon_rs_about.png" className="profile-pic" />
+              <h3> Sharon Miller</h3>
+            </Col>
+            <Col xs={12} sm={4} className="person-wrapper">
+              <Image src="https://s3.amazonaws.com/rugbyscout-two/alex_rs_about.png" className="profile-pic" />
+              <h3> Alex Hanson</h3>
+            </Col>
+          </Row>
+        </Grid >
+      </section >
 
     );
   }
