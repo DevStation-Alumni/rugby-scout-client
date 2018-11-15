e234b13fe603432fa5fdc89f5b079a4f9926683d
Sharon Miller <sharon@sharonmillercreative.com>
2018-11-15 14:32:08 -0800

    styling to nav

--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -13,7 +13,7 @@ export default class CustomNav extends React.Component {
   render() {
     let { goToAbout, goToLanding, goToLogin, goToSignup, goToProfile } = this.props.actions;
     return (
-      <Navbar className="sticky-nav" default collapseOnSelect>
+      <Navbar className="sticky-nav" inverse collapseOnSelect>
         <Navbar.Header>
           <Navbar.Brand>
             <Image src="https://s3.amazonaws.com/rugbyscout-two/rslogoonline.svg" className="logo" />
