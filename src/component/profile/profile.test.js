f942b3232885510423e85b6be063d7428c2db453
Concross <cacrossley22@gmail.com>
2018-11-15 01:22:01 -0800

    test handleChange method in profile component

--- src/component/profile/profile.test.js
+++ src/component/profile/profile.test.js
@@ -7,7 +7,7 @@ import 'jest';
 Enzyme.configure({ adapter: new Adapter });
 import ProfileContainer from './profile';
 
-describe('Profile Component', () =>{
+describe('Profile Component', () => {
 
   test('Profile component exists', () => {
 
@@ -15,4 +15,73 @@ describe('Profile Component', () =>{
 
     expect(wrapper.exists()).toBeTruthy();
   });
+
+  test('should show changes on firstName input', () => {
+
+    let onChange = sinon.spy(ProfileContainer.prototype, 'handleChange');
+    let wrapper = Enzyme.shallow(<ProfileContainer />);
+    const event = {
+      target: {
+        name: 'firstName',
+        value: 'Slardy',
+      },
+    };
+
+    wrapper.find('input[name="firstName"]').simulate('change', event);
+
+    expect(onChange.callCount).toBe(1);
+    onChange.restore();
+  });
+
+  test('should show changes on lastName input', () => {
+
+    let onChange = sinon.spy(ProfileContainer.prototype, 'handleChange');
+    let wrapper = Enzyme.shallow(<ProfileContainer />);
+    const event = {
+      target: {
+        name: 'lastName',
+        value: 'Bartfast',
+      },
+    };
+
+    wrapper.find('input[name="lastName"]').simulate('change', event);
+
+    expect(onChange.callCount).toBe(1);
+    onChange.restore();
+  });
+
+  test('should show changes on role input', () => {
+
+    let onChange = sinon.spy(ProfileContainer.prototype, 'handleChange');
+    let wrapper = Enzyme.shallow(<ProfileContainer />);
+    const event = {
+      target: {
+        name: 'role',
+        value: 'player',
+      },
+    };
+
+    wrapper.find('input[name="role"]').simulate('change', event);
+
+    expect(onChange.callCount).toBe(1);
+    onChange.restore();
+  });
+
+  test('should show changes on bio input', () => {
+
+    let onChange = sinon.spy(ProfileContainer.prototype, 'handleChange');
+    let wrapper = Enzyme.shallow(<ProfileContainer />);
+    const event = {
+      target: {
+        name: 'bio',
+        value: 'Hi, I am Slardy!',
+      },
+    };
+
+    wrapper.find('textarea[name="bio"]').simulate('change', event);
+
+    expect(onChange.callCount).toBe(1);
+    onChange.restore();
+  });
+
 });
