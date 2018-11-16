e611ce431e2d092d6915c3bfdcde0900a7d9b0bf
Sharon Miller <sharon@sharonmillercreative.com>
2018-11-15 22:57:22 -0800

    added grid to profile and profile item

--- src/component/profile-detail/profileDetail.js
+++ src/component/profile-detail/profileDetail.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Image } from 'react-bootstrap';
+import { Image, Grid, Col, Row } from 'react-bootstrap';
 import StatsView from '../stats-view/stats-view';
+
 
 export default class ProfileDetail extends React.Component {
   constructor(props) {
@@ -11,16 +12,30 @@ export default class ProfileDetail extends React.Component {
     const { profile, stats } = this.props.profileDetail ? this.props.profileDetail : this.props;
 
     return (
+
       <div className="profilePreview">
-        <h1>Profile Preview</h1>
-        <Image src={profile.imageSrc} responsive />
-        <h1>{profile.firstName + ' ' + profile.lastName}</h1>
-        <h2>{profile.position}</h2>
-        <p>{profile.bio}</p>
-        <div className="statsView">
-          {stats ? <StatsView stats={stats[0]} /> : null}
-        </div>
-      </div>
+        <Grid>
+          <Row>
+            <Col xs={12} sm={2} className="profile-photo">
+              <h1>Profile Preview</h1>
+              <Image src={profile.imageSrc} responsive />
+            </Col>
+            <Col xs={12} sm={10} className="profile-info">
+              <h1>{profile.firstName + ' ' + profile.lastName}</h1>
+              <h2>{profile.position}</h2>
+              <p>{profile.bio}</p>
+            </Col>
+
+            <div className="statsView">
+              <Row>
+                <Col xs={12} sm={12} className="stats-view">
+                  {stats ? <StatsView stats={stats[0]} /> : null}
+                </Col>
+              </Row>
+            </div>
+          </Row>
+        </Grid>
+      </div >
     );
   }
 }
