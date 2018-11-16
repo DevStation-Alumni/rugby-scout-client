7d5f752241fc23a0539fb2f4fdc7d8851d62298a
Sharon Miller <sharon@sharonmillercreative.com>
2018-11-15 19:11:09 -0800

    added styling

--- src/component/slideshow/slideshow.js
+++ src/component/slideshow/slideshow.js
@@ -5,7 +5,7 @@ class Slideshow extends React.Component {
   render() {
     return (
       <div className="carousel-container">
-        <Carousel>
+        <Carousel indicators="false">
           <Carousel.Item>
             <img width={900} height={300} alt="900x300" src="https://s3.amazonaws.com/rugbyscout-two/slideshow/590abead6185c+Cropped+Cropped.jpg" />
 
