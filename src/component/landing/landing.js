12860469edbede7162bb87a043056aa0b5a4813e
Sharon Miller <sharon@sharonmillercreative.com>
2018-11-15 21:47:20 -0800

    added styling to landing

--- src/component/landing/landing.js
+++ src/component/landing/landing.js
@@ -44,13 +44,14 @@ export default class LandingContainer extends React.Component {
             <Row className="landing-row-2">
               <Col xs={0} sm={2}></Col>
               <Col xs={12} sm={1} className="sidebar-section">
+                <h1>Top 10 Rankings</h1>
                 <ul>
                   {this.renderTopTen(results)}
                 </ul>
               </Col>
-              <Col xs={12} sm={7}>
+              <Col className="button-section" xs={12} sm={7}>
                 <ActionButton className="landing-buttons" src="https://s3.amazonaws.com/rugbyscout-two/landingbuttons/coach.jpg" api={this.props.api} query='player' />
-                <ActionButton className="landing-buttons" src="https://s3.amazonaws.com/rugbyscout-two/landingbuttons/player.jpg" api={this.props.api} query='coach' />
+                <ActionButton iclassName="landing-buttons" src="https://s3.amazonaws.com/rugbyscout-two/landingbuttons/player.jpg" api={this.props.api} query='coach' />
               </Col>
               <Col xs={0} sm={2}></Col>
             </Row>
