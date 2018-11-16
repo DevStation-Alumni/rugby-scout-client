b22770f0affecc30af62ca0d68a6586a7c510575
Concross <cacrossley22@gmail.com>
2018-11-15 16:02:44 -0800

    profile detail view renders static hello world

---
+++ src/component/profile-detail/profileDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class ProfileDetail extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Hello, ProfileDetail!</h1>
+    );
+  }
+}
